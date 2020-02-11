0c43096c692093674385fd9500df6ce0f8a036f1
Sergei Vasilevkin <sergei_vasilevkin@epam.com>
2020-02-11 13:40:09 +0300

    Add Constants and update db schema.

---
+++ Notes/Notes/model/Constants.swift
@@ -0,0 +1,22 @@
+//
+//  Constants.swift
+//  Notes
+//
+//  Created by Sergey Vasilevkin on 11/02/2020.
+//  Copyright © 2020 Sergei Vasilevkin. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    static let noteCellReuseIdentifier = "NoteCell"
+    
+    // CoreData entity Note
+    struct Note {
+        static let name = "Note"
+        static let noteId = "noteId"
+        static let title = "title"
+        static let text = "text"
+        static let timestamp = "timestamp"
+    }
+}
