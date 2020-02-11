04b2950660e06e2a614bfa5b4f6db7551a598aed
Sergei Vasilevkin <sergei_vasilevkin@epam.com>
2020-02-11 09:20:54 +0300

    Create a new project in Xcode.

---
+++ Notes/Notes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Sergey Vasilevkin on 11/02/2020.
+//  Copyright © 2020 Sergei Vasilevkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
